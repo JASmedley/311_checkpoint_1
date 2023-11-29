994c5cfb4637fe3d4fe5f8a6a6fe4d78d5577edd
JASmedley <jasmine.a.smedley@gmail.com>
2023-11-28 20:54:01 -0600

    finishing assignment

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 let express = require("express");
 const port = 4000;
-
-express().use(express.json());
-express().use(express.static("./public/index.html"));
-express().use(require("./routes/recipeRoutes"));
-express().listen(port, () => {
+let app = express()
+app.use(express.json());
+app.use(express.static("./public/index.html"));
+app.use(require("./routes/recipeRoutes"));
+app.listen(port, () => {
   console.log('App is listening on:', port)
 });
